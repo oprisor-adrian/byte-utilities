845aa859bf86a5f74504eaefa0f1ef0300337931
oprisor-adrian <oprisoradrian@gmail.com>
2023-12-26 15:23:46 +0000

    Update the license

--- include/byte_vector.h
+++ include/byte_vector.h
@@ -1,3 +1,21 @@
+/* 
+  Copyright (C) 2023 Oprișor Adrian-Ilie
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   
+  Contact: contact@dev-adrian.com
+*/
 #ifndef BYTE_UTILS_BYTE_VECTOR_H_
 #define BYTE_UTILS_BYTE_VECTOR_H_
 
